8bbcc9f780dcf725082e6d6f55f33b7a87bc45e4
MuhammadHussien1896 <muhammadhussien1896@gmail.com>
2022-04-19 18:00:58 +0200

    adding v3 migration

---
+++ Sho8lana/Migrations/20220419155947_v3.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sho8lana.Migrations
+{
+    public partial class v3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CategoryImg",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryImg",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Categories");
+        }
+    }
+}
