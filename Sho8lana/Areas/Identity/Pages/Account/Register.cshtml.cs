56efbf71e7bba34fe7c8c9348024b73da8fdeb01
MahmoudAbdElhamed1994 <100537745+MahmoudAbdElhamed1994@users.noreply.github.com>
2022-04-21 03:33:02 +0200

    correcting merge

--- Sho8lana/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Sho8lana/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -168,11 +168,11 @@ namespace Sho8lana.Areas.Identity.Pages.Account
                     Area=Input.Area
 
                 };
-=======
-                var user = CreateUser();
+
+                user = CreateUser();
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
->>>>>>> master
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
