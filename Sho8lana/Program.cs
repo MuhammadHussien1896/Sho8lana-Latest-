73c1518685dd8801a2f2c301737690fbd045754d
Mahmoud771771 <mahmoud771771@github.com>
2022-04-21 21:06:59 +0200

    add temp1

--- Sho8lana/Program.cs
+++ Sho8lana/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("Connection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseLazyLoadingProxies().UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
