461cb07686d3d3e20682b99b3b88d356d6d53292
MuhammadHussien1896 <muhammadhussien1896@gmail.com>
2022-04-22 02:02:50 +0200

    adding IsAccepted into service table

---
+++ Sho8lana/Migrations/20220421235932_adding_property_IsAccepted_into_service.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sho8lana.Migrations
+{
+    public partial class adding_property_IsAccepted_into_service : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAccepted",
+                table: "Services",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAccepted",
+                table: "Services");
+        }
+    }
+}
