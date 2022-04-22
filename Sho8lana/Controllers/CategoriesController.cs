e455d74b530152a4eadb8a12c238ca6f369eed32
MahmoudAbdElhamed1994 <100537745+MahmoudAbdElhamed1994@users.noreply.github.com>
2022-04-22 23:45:19 +0200

    adding hiring view

--- Sho8lana/Controllers/CategoriesController.cs
+++ Sho8lana/Controllers/CategoriesController.cs
@@ -15,27 +15,52 @@ namespace Sho8lana.Controllers
 
         public async Task<IActionResult> Index(int id)
         {
-            var services =await _context.Services.GetAllByCategory(id);
+
+            var services = await _context.Services.GetAllBy(s => s.IsFreelancer && s.IsAccepted && s.CategoryId == id);
             ViewBag.Categories = await _context.Categories.GetAll();
             ViewBag.Id = id;
             return View(services);
         }
-        
-        public async Task<IActionResult> Search(string text,int? categoryId)
+
+        public async Task<IActionResult> Search(string text)
         {
+            ViewBag.Text = text;
+            ViewBag.Categories = await _context.Categories.GetAll();
+            var services = await _context.Services
+                .GetAllBy(s => s.Description.Contains(text)&& s.IsFreelancer && s.IsAccepted);
+            if (services == null) return NotFound();
+            return View(services);
+        }
+        [HttpGet]
+        [ActionName("Search")]
+        public async Task<IActionResult> SearchByCategory(string text, int? categoryId)
+        {
+            ViewBag.Text = text;
+            ViewBag.Id = categoryId;
             ViewBag.Categories = await _context.Categories.GetAll();
             if (categoryId == null)
             {
-                var services = await _context.Services.GetAllBy(s => s.Description.Contains(text));
-                if(services==null) return NotFound();
-                return View("index", services);
+                var services = await _context.Services
+                    .GetAllBy(s => s.Description.Contains(text)&& s.IsFreelancer && s.IsAccepted);
+                if (services == null) return NotFound();
+                return View(services);
             }
             else
             {
-                var services = await _context.Services.GetAllBy(s => s.Description.Contains(text) && s.Category.CategoryId ==categoryId);
+                var services = await _context.Services
+                    .GetAllBy(s => s.Description.Contains(text) &&s.IsFreelancer && s.IsAccepted && s.Category.CategoryId == categoryId);
                 if (services == null) return NotFound();
-                return View("index", services);
+                return View(services);
             }
+        }
+        public async Task<IActionResult> Hiring(int id)
+        {
+            var services = await _context.Services.GetAllBy(s => s.IsFreelancer==false && s.IsAccepted && s.CategoryId == id);
+            ViewBag.Categories = await _context.Categories.GetAll();
+            ViewBag.Id = id;
+            return View(services);
+
         }
     }
 }
+
