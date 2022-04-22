bceef658abaf7e7d250c58de48491a7edecb835b
Mahmoud771771 <mahmoud771771@github.com>
2022-04-22 02:23:37 +0200

    Create Login

--- Sho8lana/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Sho8lana/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Sho8lana.Models;
+using System.Net.Mail;
 
 namespace Sho8lana.Areas.Identity.Pages.Account
 {
@@ -108,11 +109,12 @@ namespace Sho8lana.Areas.Identity.Pages.Account
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
+            var userName = new EmailAddressAttribute().IsValid(Input.Email) ? new MailAddress(Input.Email).User : Input.Email;
             if (ModelState.IsValid)
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(userName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
