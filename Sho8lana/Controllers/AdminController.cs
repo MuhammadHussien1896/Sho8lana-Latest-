bb47570662c85f58453f11298d77b28721b59324
Mahmoud771771 <mahmoud771771@github.com>
2022-04-25 01:54:35 +0200

    Add Admin Controller Empty

---
+++ Sho8lana/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sho8lana.Unit_Of_Work;
+
+namespace Sho8lana.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IUnitOfWork _context;
+
+        public AdminController(IUnitOfWork context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
