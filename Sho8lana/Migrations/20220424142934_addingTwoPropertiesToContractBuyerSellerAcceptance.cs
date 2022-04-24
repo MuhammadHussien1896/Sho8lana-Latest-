9059c22b5518b2666c410e668cbf640d024b26fd
Muhammad Hussien <muhammadhussien1896@gmail.com>
2022-04-24 18:36:53 +0200

    accept service request done added two properties to contract

---
+++ Sho8lana/Migrations/20220424142934_addingTwoPropertiesToContractBuyerSellerAcceptance.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sho8lana.Migrations
+{
+    public partial class addingTwoPropertiesToContractBuyerSellerAcceptance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notification_AspNetUsers_CustomerId",
+                table: "Notification");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Notification",
+                table: "Notification");
+
+            migrationBuilder.RenameTable(
+                name: "Notification",
+                newName: "Notifications");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notification_CustomerId",
+                table: "Notifications",
+                newName: "IX_Notifications_CustomerId");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "BuyerAccepted",
+                table: "Contracts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SellerAccepted",
+                table: "Contracts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Notifications",
+                table: "Notifications",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_AspNetUsers_CustomerId",
+                table: "Notifications",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_AspNetUsers_CustomerId",
+                table: "Notifications");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Notifications",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "BuyerAccepted",
+                table: "Contracts");
+
+            migrationBuilder.DropColumn(
+                name: "SellerAccepted",
+                table: "Contracts");
+
+            migrationBuilder.RenameTable(
+                name: "Notifications",
+                newName: "Notification");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notifications_CustomerId",
+                table: "Notification",
+                newName: "IX_Notification_CustomerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Notification",
+                table: "Notification",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notification_AspNetUsers_CustomerId",
+                table: "Notification",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
