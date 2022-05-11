1297325f6bb4e6f9a701bec8a48fd3e1aa059811
MahmoudAbdElhamed1994 <100537745+MahmoudAbdElhamed1994@users.noreply.github.com>
2022-05-11 02:33:44 +0200

    fix errors

--- Sho8lana/Controllers/CustomerController.cs
+++ Sho8lana/Controllers/CustomerController.cs
@@ -525,10 +525,11 @@ namespace Sho8lana.Controllers
             {
                 return LocalRedirect("~/Identity/Account/AccessDenied");
             }
-            var boughtservices = (await context.Payments.GetAllEagerLodingAsync(s => s.Contract.BuyerId == customerId && s.Contract.SericeOwnerId != customerId,new string[] { "Contract", "Contract.Service", "Customer" })).OrderByDescending(s => s.CreatedDate);
-            var soldservices = (await context.Payments.GetAllEagerLodingAsync(s => s.Contract.SellerId == customerId && s.Contract.SericeOwnerId == customerId && s.Contract.IsDone == true, new string[] { "Contract", "Contract.Service", "Customer" })).OrderByDescending(s => s.CreatedDate);
+            var payments=await context.Payments.GetAllEagerLodingAsync(s => s.CustomerId == customerId||s.Contract.CustomerId==customerId, new string[] { "Contract", "Contract.Service", "Customer" });
+            var boughtservices = payments.Where(s => s.Contract.BuyerId == customerId && s.Contract.SericeOwnerId != customerId).OrderByDescending(s => s.CreatedDate);
+            var soldservices = payments.Where(s => s.Contract.SellerId == customerId && s.Contract.SericeOwnerId == customerId && s.Contract.IsDone == true).OrderByDescending(s => s.CreatedDate);
+            var orders = payments.Where(s => s.CustomerId == customerId && s.Contract.SericeOwnerId == customerId).OrderByDescending(s => s.CreatedDate);
             var charges = (await context.BalanceCharges.GetAllEagerLodingAsync(s => s.CustomerId == customerId, new string[] { "Customer" })).OrderByDescending(s => s.CreatedDate);
-            var orders = (await context.Payments.GetAllEagerLodingAsync(s => s.CustomerId == customerId && s.Contract.SericeOwnerId == customerId, new string[] { "Contract", "Contract.Service", "Customer" })).OrderByDescending(s => s.CreatedDate);
             var model = new CustomerBalanceDetailsViewModel()
             {
                 balanceCharges = charges,
