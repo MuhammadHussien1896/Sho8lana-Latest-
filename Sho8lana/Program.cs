4f87dc0fca329b3c6a3db6e482363a2d2ec43c8f
MahmoudAbdElhamed1994 <100537745+MahmoudAbdElhamed1994@users.noreply.github.com>
2022-05-21 14:19:55 +0200

    fix bugs

--- Sho8lana/Program.cs
+++ Sho8lana/Program.cs
@@ -21,7 +21,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("SmarterAsp");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options
     //.UseLazyLoadingProxies()
